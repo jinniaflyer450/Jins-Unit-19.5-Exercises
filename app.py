e7e1e39e7ee1636c45633b5560021e6cd691992c
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-07-13 11:04:05 -0500

    added new routes to handle setting up the board

--- app.py
+++ app.py
@@ -4,22 +4,33 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='catdog'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 
 debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
 @app.route('/')
-def display_board():
+def display_start():
+    return render_template('start_game.html')
+
+@app.route('/game-setup')
+def setup_board():
     board = boggle_game.make_board()
     session['board'] = board
     session['guesses'] = []
-    return render_template('board.html', board=board)
+    return redirect('/active-game')
 
-@app.route('/guess', methods=['POST'])
+@app.route('/active-game')
+def display_current_game():
+    return render_template('board.html', board=session['board'])
+
+@app.route('/guess')
 def submit_guess():
-    guess = request.form['guess']
-    return redirect('/')
+    guess = request.args['guess']
+    if guess is not None:
+        session['guesses'].append(guess)
+    return redirect('/active-game')
 
 
 #https://werkzeug.palletsprojects.com/en/2.0.x/exceptions/#
--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 const $guessForm = $('#guess-form');
 $guessForm.on('submit', async function(evt){
     evt.preventDefault();
-    let response = await axios.post('/guess', {'guess': $('#guess').val()});
+    let response = await axios.get('/guess', {'params': {'guess': $('#guess').val()}});
     return response;
 })
