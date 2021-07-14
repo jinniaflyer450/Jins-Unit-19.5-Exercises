37aafdd3c42bfb159206a1e3a02d33fcc141cc8e
Elizabeth Watson <elizabethwats0n450@gmail.com>
2021-07-14 09:52:25 -0500

    added docstrings to view functions

--- app.py
+++ app.py
@@ -10,12 +10,16 @@ final_score = None
 
 @app.route('/')
 def display_start():
+    """Initializes the game-count and high-score values, then returns the HTML page that gives instructions and allows
+    starting the game."""
     session['game-count'] = 0
     session['high-score'] = 0
     return render_template('start_game.html')
 
 @app.route('/setup-game')
 def setup_board():
+    """Initializes a new board and includes it in the session. Covers for if game-count and high-score were not initialized in session
+    and redirects to the /show-board route."""
     board = boggle_game.make_board()
     session['board'] = board
     if session.get('game-count', None) == None:
@@ -26,12 +30,18 @@ def setup_board():
 
 @app.route('/show-board')
 def show_board():
+    """If board is not initialized in session, redirects to /setup-game so that board can be initialized. Otherwise,
+    returns the template that shows the board and allows you to play the game."""
     if session.get('board', None) == None:
         return redirect('/setup-game')
     return render_template('board.html', board=session['board'])
 
 @app.route('/guess')
 def submit_guess():
+    """If board is not initialized in session, redirects to /setup-game so that board can be initialized. If no guess was sent,
+    redirects to /show-board to allow a guess. If game-count and high-score are not initialized in session, initializes them.
+    Otherwise, retrieves guess from the query string, checks if the guess is valid and if it is if it is on the board, then returns
+    the results of checking as JSON."""
     if session.get('board', None) == None:
         return redirect('/setup-game')
     elif request.args.get('guess', None) == None:
@@ -47,6 +57,9 @@ def submit_guess():
 
 @app.route('/stop-timer')
 def stop_timer():
+    """After the time for the game has run out, updates game-count in session (adding 1) and final_score as a global variable 
+    from the query string. If final_score is the new high score, updates high-score in session. Then, sends data in JSON that
+    allows for automatic redirection to the /post-game route."""
     global final_score
     session['game-count'] += 1
     final_score = int(request.args['finalScore'])
@@ -56,6 +69,8 @@ def stop_timer():
 
 @app.route('/post-game')
 def end_game():
+    """Returns the HTML page that displays that the game has ended, the number of times played, the high score, and the
+    final score."""
     return render_template('post_game.html', finalScore = final_score)
 
 #Got some help with jsonify here: https://www.kite.com/python/docs/flask.jsonify
